54147410950d31ab279a3c11b16b154fd16f1226
Matheus Moraes <mmoraes.job@outlook.com>
2025-12-05 13:37:39 -0300

    Organização
    Realizado limpeza e organização do código, agora é possivel ler com mais clareza

--- Robo/RoboMouse.py
+++ Robo/RoboMouse.py
@@ -1,13 +1,22 @@
 import pyautogui
+from time import sleep
 
-pyautogui.keyDown('alt')
-pyautogui.press(['tab'])
-pyautogui.keyUp('alt')
-pyautogui.press(38 * ['tab'])
+
+pyautogui.PAUSE = 0.5 
+
+pyautogui.hotkey('alt', 'tab') 
+sleep(1) 
+
+pyautogui.press('tab', presses=38, interval=0.01) 
 pyautogui.press('enter')
 
+sleep(1)
 
-time
-pyautogui.click('Robo/proximo.png')
+pyautogui.press('tab', presses=14, interval=0.01)
+pyautogui.press('enter')
 
-#pyautogui.move(0,40)
+sleep(1.5)
+
+pyautogui.press('tab', presses=3, interval=0.1)
+sleep(1)
+pyautogui.press('enter')
