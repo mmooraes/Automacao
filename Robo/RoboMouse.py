e69666f3203b918d972e3a1fd4d83a73fcf2581b
Matheus Moraes <mmoraes.job@outlook.com>
2025-12-05 12:46:56 -0300

    add cod

---
+++ Robo/RoboMouse.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.keyDown('alt')
+pyautogui.press(['tab'])
+pyautogui.keyUp('alt')
+pyautogui.press(38 * ['tab'])
+pyautogui.press('enter')
+
+
+time
+pyautogui.click('Robo/proximo.png')
+
+#pyautogui.move(0,40)
